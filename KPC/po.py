770157f869e505f70dd628dc9b83e7fa18fd422f
Jihui Choi <jhchoi@koost.net>
2018-04-09 23:51:47 +0900

    misc 코드 정리

--- KPC/po.py
+++ KPC/po.py
@@ -72,38 +72,46 @@ class ignore:
         self.ignore_file = ''
 
     def action(self, param):
-        from os.path import expanduser
-        ignore_file = expanduser("~") + '/.ko-po-check-ignore'
-        if param == 'yes':
-            print('ignore 파일 읽어오기')
-            if not os.path.exists(ignore_file):
-                open(ignore_file, 'w').close()
-            with open(ignore_file) as f:
-                #파일을 줄별로 가져와 리스트에 넣는다. 이때 줄에서 #이후는 주석으로 간주해 '#' 이후 문자열은 제외한다.
-                ignore_keyids = [n.split('#', 1)[0].strip() for n in f.read().splitlines()]
-            return ignore_keyids
-        elif 'file' in param:
-            print('ignore 파일 읽어오기')
-            ignore_file = param.replace('file:','')
+        #ignore file 을 지정할 때와 안할 때 구분
+        if 'file' in param:
+            ignore_file = param.split(':',1)[1]
             if ignore_file[0] != '/':
                 ignore_file = os.getcwd() + '/' + ignore_file
             if not os.path.exists(ignore_file):
                 print('%s 파일이 없습니다.' % ignore_file)
                 sys.exit(1)
+        else:
+            ignore_file = os.getcwd() + '/.ko-po-check-ignore'
+            if not os.path.exists(ignore_file):
+                open(ignore_file, 'w').close()
+
+        #param 에 따른 동작 구분
+        if 'default' == param:
+            print('ignore 기본 파일 읽어오기')
             with open(ignore_file) as f:
                 #파일을 줄별로 가져와 리스트에 넣는다. 이때 줄에서 #이후는 주석으로 간주해 '#' 이후 문자열은 제외한다.
                 ignore_keyids = [n.split('#', 1)[0].strip() for n in f.read().splitlines()]
             return ignore_keyids
+        elif 'file' in param:
+            print('%s 파일 읽어오기' % ignore_file)
+            with open(ignore_file) as f:
+                #파일을 줄별로 가져와 리스트에 넣는다. 이때 줄에서 #이후는 주석으로 간주해 '#' 이후 문자열은 제외한다.
+                ignore_keyids = [n.split('#', 1)[0].strip() for n in f.read().splitlines()]
+            return ignore_keyids
+
+        #add 와 clear 는 기본 파일에만 적용된다.
         elif 'add' in param:
-            print('무시 목록에 추가')
+            print('ignore 기본 파일에 추가')
             f = open(ignore_file,"a")
             for n in param.replace('add:','').split(','):
                 f.write(n + "\n")
             f.close()
+            f = open(ignore_file,"rb").read()
+            print(f.decode("utf-8"))
             sys.exit(1)
         elif param == 'clear':
-            print('무시 목록 초기화')
+            print('ignore 기본 파일 초기화')
             if os.path.exists(ignore_file):
                 os.remove(ignore_file)
-            open(ignore_file, 'w').close()
+                open(ignore_file, 'w').close()
             sys.exit(1)
